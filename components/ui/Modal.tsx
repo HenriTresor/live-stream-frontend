a4c081695f6b67c8b0d95b2ad217ef2985855026
henritresor <shimwamanahenritresor@gmail.com>
2023-10-04 21:32:13 +0200

    modified modal

--- components/ui/Modal.tsx
+++ components/ui/Modal.tsx
@@ -1,43 +1,78 @@
-import { useState, Fragment } from 'react'
+import { dialogTypes } from '@/providers/ModalProvider';
 import { Dialog, Transition } from '@headlessui/react'
-import { dialogTypes } from '@/providers/ModalProvider'
+import { Fragment, useState } from 'react'
 
 type Props = {
+    title: string,
+    description: string;
+    nextAction: (vl: any) => void;
+    dialogContent: dialogTypes,
     isOpen: boolean,
-    dialogContent: dialogTypes | null,
-    setIsOpen: (vl: boolean) => void,
-    nextAction?: (evt: any) => void
+    setIsOpen: (vl: boolean) => void;
 }
 
-function MyDialog({ isOpen, dialogContent, setIsOpen, nextAction }: Props) {
+export default function MyModal({ title, description, nextAction, dialogContent, isOpen, setIsOpen }: Props) {
+
 
     return (
-        <Transition
-            show={isOpen}
-            enter="transition duration-100 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
-            as={Fragment}
-        >
-            <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-                <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-                <Dialog.Panel>
-                    <Dialog.Title>{dialogContent?.title}</Dialog.Title>
-                    <Dialog.Description>
-                        {dialogContent?.description}
-                    </Dialog.Description>
+        <>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
 
-                    {dialogContent?.children}
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900"
+                                    >
+                                        {title}
+                                    </Dialog.Title>
+                                  {dialogContent?.children}
 
-                    <button onClick={nextAction}>Continue</button>
-                    <button onClick={() => setIsOpen(false)}>Cancel</button>
-                </Dialog.Panel>
-            </Dialog>
-        </Transition>
+                                    <div className="mt-4 flex gap-4">
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-900 px-4 py-2 text-sm font-medium text-blue-100 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                            onClick={nextAction} 
+                                        >
+                                            Continue
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                            onClick={() => setIsOpen(false)}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                    
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
     )
 }
-
-export default MyDialog
--- components/views/Header.tsx
+++ components/views/Header.tsx
@@ -1,12 +1,17 @@
+'use client'
 import { navListItems } from '@/utils/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import { ModalContext } from '@/providers/ModalProvider'
+import Signup from './forms/Signup'
 
 type Props = {}
 
 function Header({ }: Props) {
+
+    const { setDialogContent, setIsOpen } = React.useContext(ModalContext)
     return (
         <div className='flex p-2 items-center w-full h-auto justify-between pl-10 pr-10 '>
             <Image
@@ -25,7 +30,17 @@ function Header({ }: Props) {
                 }
             </div>
 
-            <Button className='capitalize bg-orange-500 text-white'>get started</Button>
+            <Button className='capitalize bg-orange-500 text-white'
+                onClick={() => {
+                    setIsOpen(true)
+                    setDialogContent({
+                        title: "Create Account",
+                        description: 'Sign up with your email for registration ',
+                        children: <Signup />,
+                        nextAction: () => { },  
+                    })
+                }}
+            >get started</Button>
         </div>
     )
 }
