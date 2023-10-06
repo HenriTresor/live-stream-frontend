57798bd02e73a086b468de2dccc8c0aa44fbcf4c
henritresor <shimwamanahenritresor@gmail.com>
2023-10-06 09:01:07 +0200

    fixed build

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,10 +2,7 @@ import Aside from '@/components/views/Home/Aside'
 import Header from '@/components/views/Home/Header'
 import React from 'react'
 import '../../app/globals.css'
-import Body from '@/components/views/Home/Body'
 import { Montserrat } from 'next/font/google'
-import { Metadata } from 'next'
-import Head from 'next/head'
 
 
 const inter = Montserrat({ subsets: ['latin'] })
@@ -15,17 +12,16 @@ type Props = {
     children: React.ReactNode
 }
 
-function layout({ children }: Props) {
+function Layout({ children }: Props) {
     return (
-
-        <div className={`${inter.className} m-0 p-0 flex`}>
+        <section className={`${inter.className} m-0 p-0 flex`}>
             <Aside />
             <div className='w-[80%]'>
                 <Header />
                 {children}
             </div>
-        </div>
+        </section>
     )
 }
 
-export default layout
+export default Layout
