adf63f7d4b481339ecd1e801e74df112d40ca47f
henritresor <shimwamanahenritresor@gmail.com>
2023-10-06 07:58:33 +0200

    added active property to the liks

--- components/views/Home/Aside.tsx
+++ components/views/Home/Aside.tsx
@@ -1,13 +1,16 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import { homeSideMenu } from '@/utils/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { FaStream, FaPlus } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
 
 type Props = {}
 
 function Aside({ }: Props) {
+  const pathname = usePathname()
   return (
     <div className='p-2 w-[20%] h-[100dvh] border flex flex-col justify-between'>
       <div className='w-full flex flex-col gap-4'>
@@ -28,7 +31,7 @@ function Aside({ }: Props) {
         <div className='flex flex-col p-2 mt-3'>
           {
             homeSideMenu.map(item => (
-              <Link key={item.name} href={item.link} className='w-full p-2 flex items-center justify-normal gap-5 font-bold bg-gray-100 border-r-2 border-r-orange-600 mb-3 text-[#00000082] cursor-pointer'>
+              <Link key={item.name} href={item.link} className={`w-full p-2 flex items-center justify-normal gap-5 font-bold ${pathname === item.link && `bg-gray-100 border-r-2 border-r-orange-600`} mb-3 text-[#00000082] cursor-pointer`}>
                 {item.icon()}
                 {item.name}
               </Link>
@@ -38,7 +41,7 @@ function Aside({ }: Props) {
       </div>
 
       <div className='w-full flex flex-col'>
-        
+
         <Button className='flex items-center gap-5'>
           <FaPlus />
           Add a Friend</Button>
